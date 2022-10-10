f2aaa30bcb6f95ef1a79a1e1569287416d1dc0d6
Srinivas <sganta@albany.edu>
2022-10-09 22:18:49 -0400

    using http over https

--- src/news.js
+++ src/news.js
@@ -5,7 +5,7 @@ const News = () => {
 
     useEffect(() => {
         fetch(
-            "https://newsapi.org/v2/top-headlines?country=us&apiKey=11bac121ba934559837336398c4bc599")
+            "http://newsapi.org/v2/top-headlines?country=us&apiKey=11bac121ba934559837336398c4bc599")
             .then((res) => res.json())
             .then((res) => {
                 const filteredData = res?.articles?.map((article) => ({
