2c0d317b451347c59a105907bc701c4fe77f4b4e
Srinivas <sganta@albany.edu>
2022-10-17 16:22:58 -0400

    using rapid api instead of news api

--- src/news.js
+++ src/news.js
@@ -4,14 +4,26 @@ const News = () => {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b507b8f252mshfb2e60d36346405p19922bjsnb8cb16024e30',
+                'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+            }
+        };
         fetch(
-            "http://newsapi.org/v2/top-headlines?country=us&apiKey=11bac121ba934559837336398c4bc599")
+            'https://crypto-news-live3.p.rapidapi.com/news',
+            options
+            )
             .then((res) => res.json())
             .then((res) => {
-                const filteredData = res?.articles?.map((article) => ({
+                console.log('resp', res);
+                res = res.slice(0,10);
+                console.log('after resp', res);
+                const filteredData = res?.map((article) => ({
                     title: article.title,
-                    summary: article.description,
-                    author: article.author,
+                    // summary: article.description,
+                    // author: article.author,
                     link: article.url,
                 }));
                 setArticles(filteredData);
@@ -26,15 +38,15 @@ const News = () => {
         <h3 className="container">Today's Headlines</h3>
         <div className="container">
             {articles.length > 0 ? (
-                articles.map((article) => {
+                articles.map((article, index) => {
                     return (
-                        <div className="card col my-2">
+                        <div className="card col my-2" key={index}>
                             <div className="card-body">
                                 <h5 className="card-title">{article.title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">
+                                {/* <h6 className="card-subtitle mb-2 text-muted">
                                     {article.author}
-                                </h6>
-                                <p className="card-text">{article.summary}</p>
+                                </h6> */}
+                                {/* <p className="card-text">{article.summary}</p> */}
                                 <a href={article.link} className="card-link">
                                     {article.link}
                                 </a>
